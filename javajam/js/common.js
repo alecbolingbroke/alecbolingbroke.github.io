cdf04b1131c519444a3d6f77cbf7fa497f28c442
alecbolingbroke <87875824+alecbolingbroke@users.noreply.github.com>
2023-04-10 20:14:15 -0600

    Assignment 10
    Adding a Catalog Page for the Online Store

--- javajam/js/common.js
+++ javajam/js/common.js
@@ -2,4 +2,7 @@ $(document).ready(function () {
     $('#mug').click(function () {
         alert("JavaJam Mug Club Members get a 10% discount on each cup of coffee!");
     });
+    $('#discount').click(function () {
+        alert("Sign up to be a JavaJam Mug Club Member and get 10% on every cup of coffee!");
+    });
 });
